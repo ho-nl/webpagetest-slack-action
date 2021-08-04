7d08c2ec6e3af27d2de5c42546544e9ae5394c52
Tim Hofman <tim@reachdigital.nl>
2021-08-04 14:32:46 +0200

    markdown variable was not defined before usage

--- index.js
+++ index.js
@@ -55,11 +55,11 @@ const retrieveResults = (wpt, testId) => {
 }
 async function renderComment(data) {
     try {
+        let markdown = await ejs.renderFile(`${__dirname}/templates/comment.md`, data);
+
         if (GH_EVENT_NAME == 'pull_request') {
             const octokit = github.getOctokit(GITHUB_TOKEN, {log: console});
             const context = github.context;
-
-            let markdown = await ejs.renderFile(`${__dirname}/templates/comment.md`, data);
             markdown
                 .replace(/\%/g, '%25')
                 .replace(/\n/g, '%0A')
